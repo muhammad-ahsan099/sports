3c1721d3561c40c5c6d99929993a84049ea45051
muhammad-ahsan099 <taseries260@gmail.com>
2023-12-14 14:29:36 +0500

    initial commit

--- App.tsx
+++ App.tsx
@@ -72,30 +72,6 @@ function App(): JSX.Element {
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <WebView source={{ uri: 'https://www.sportspotnews.com/' }} style={{ flex: 1 }} />
-      {/* <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView> */}
     </SafeAreaView>
   );
 }
